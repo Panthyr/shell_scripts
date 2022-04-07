85918ce2f105bbde57c3daceefb7f1177663f0d3
dietervansteenwegen <github@vansteenwegen.org>
2022-04-07 16:50:33 +0200

    Pull all repos in repo directory

---
+++ pull_repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+stdout=$(tempfile)
+stderr=$(tempfile)
+
+
+for d in /home/hypermaq/repos/panthyr*/ ; do
+    echo "-> Pulling for $d"
+    cd $d
+    if ! git pull </dev/null >$stdout 2>$stderr; then
+        echo "*********"
+        cat $stderr >&2
+        echo "*********"
+    fi
+    rm -f $stdout $stderr
+
+echo "DONE."
+done
