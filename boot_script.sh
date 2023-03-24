63ca6618c0da05e60c1564c64e6f0a36dab9bfa6
Panthyr <panthyr@example.com>
2023-03-24 12:28:22 +0000

    fix path

--- boot_script.sh
+++ boot_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py -B
 sleep 30
-/usr/bin/python3 -u /home/hypermaq/scripts/queue.py -a set_station_params,1
+/usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py -a set_station_params,1
