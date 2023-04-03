80619809d7b9c5d11e29569ba63c2cd654d44ba2
Panthyr <panthyr@example.com>
2023-04-03 08:39:44 +0000

    use verbose parameters instead of shorthand

--- boot_script.sh
+++ boot_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py -B
+/usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py --booted
 sleep 30
-/usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py -a set_station_params,1
+/usr/bin/python3 -u /home/panthyr/repos/panthyr_core/p_core/queue/queue.py --set_station_parameters
